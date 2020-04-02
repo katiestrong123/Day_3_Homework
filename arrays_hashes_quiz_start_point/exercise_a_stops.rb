cafddceb10712e546d7b3ad3c6ca02cc758524be
katiestrong123 <katiestrong123@gmail.com>
2020-04-02 06:37:22 +0100

    Arrays and hashes inital commit

---
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -0,0 +1,36 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+#1. Add "Edinburgh Waverley" to the end of the array
+stops.push("Edinburgh Waverley")
+
+#2. Add "Glasgow Queen St" to the start of the array
+stops.unshift("Glasgow Queen St")
+
+#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops[3]= "Polmont"
+
+#4. Print out the index position of "Linlithgow"
+p stops.index("Linlithgow")
+
+#5. Remove "Livingston" from the array using its name
+stops.delete("Livingston")
+
+#6. Delete "Cumbernauld" from the array by index
+stops.delete_at(1)
+
+#7. Print the number of stops there are in the array?
+p stops.length
+
+#8. Show as many ways as you can to return "Falkirk High" from the array?
+p stops.at(2)
+p stops[2]
+p stops.fetch(2)
+p stops.index("Falkirk High")
+
+#9. Reverse the positions of the stops in the array
+stops.reverse
+
+#10 Print out all the stops using a for loop
+for stations in stops
+    p stations
+  end 
---
+++ arrays_hashes_quiz_start_point/exercise_c_uk.rb
@@ -0,0 +1,40 @@
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
+
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+united_kingdom[1][:capital] = "Cardiff"
+
+# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+united_kingdom.push(name: "Northern_Ireland", population: "1,811,000".to_i, capital: "Belfast")
+
+# 3. Use a loop to print the names of all the countries in the UK.
+for country in united_kingdom
+  p country[:name]
+end
+
+# 4. Use a loop to find the total population of the UK.
+def total_population(array)
+  total_population = 0
+      for country in array
+        total_population += country[:population]
+        country[:population] = 0
+      end
+  return total_population
+end
+
+p total_population(united_kingdom)
